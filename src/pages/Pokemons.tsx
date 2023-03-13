45385e6fd687474fbb624da7deeef37083233196
FlorCollosso <florcollosso@gmail.com>
2023-03-13 13:38:07 -0300

    Main y Pokemons

--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,13 +1,39 @@
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import BulbasaurPic from '../assets/bulbasaur.gif';
+import styles from './pokemons.module.css';
 
 const Pokemons = () => {
     const [query, setQuery] = useState("");
     return <>
     <Header query={query} setQuery={setQuery}/>
     <main>
-        <h1>Pokemons</h1>
+        <nav>
+            <Link className={styles.listItem} to="/">
+                <img className={styles.listItemIcon} src={BulbasaurPic} alt="Bulbasaur" />
+                <div className={styles.listItemText}>
+                    <span>Bulbasaur</span>
+                    <span>001</span>
+                </div>
+            </Link>
+            <Link className={styles.listItem} to="/">
+                <img className={styles.listItemIcon} src={BulbasaurPic} alt="Bulbasaur" />
+                <div className={styles.listItemText}>
+                    <span>Ivysaur</span>
+                    <span>002</span>
+                </div>
+            </Link>
+            <Link className={styles.listItem} to="/">
+                <img className={styles.listItemIcon} src={BulbasaurPic} alt="Bulbasaur" />
+                <div className={styles.listItemText}>
+                    <span>Venusaur</span>
+                    <span>003</span>
+                </div>
+            </Link>
+        </nav>
     </main>
     <Footer/>
     </>
